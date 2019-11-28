46a432355b908f28182c34bdcba97e4ff66d4ad3
Denys <denys.cheporniuk@gmail.com>
2019-11-28 19:27:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
+/* eslint-disable indent */
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.getElementById('cars');
+
+[...table.tHead.rows][0].addEventListener('click', () => {
+  const rows = Array.from(table.rows);
+  const tableHead = [...rows[0].cells].map(item => item.textContent);
+  const index = tableHead.indexOf(event.target.textContent);
+
+  const sortTable = rows.slice(1).sort((a, b) => {
+    switch (isNaN(+a.cells[index].textContent)) {
+      case true:
+        return a.cells[index].textContent > b.cells[index].textContent
+              ? 1 : -1;
+
+      case false:
+        return a.cells[index].textContent - b.cells[index].textContent;
+
+      default:
+        return 0;
+    };
+  });
+
+  table.tBodies[0].append(...sortTable);
+});
