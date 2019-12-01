497ee437bd65c77891ff316facd3d9e651c0723a
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-01 15:20:23 +0200

    Aidinov Alexii

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.querySelector('#cars');
+
+table.addEventListener('click', handleClick);
+
+function handleClick(click) {
+  const th = click.target;
+
+  if (th.tagName !== 'TH') {
+    return;
+  }
+
+  sortColumn(th.cellIndex, th.dataset.type);
+}
+
+function sortColumn(column, type) {
+  const tbody = table.querySelector('tbody');
+  const rows = Array.from(tbody.rows);
+  let callback;
+
+  switch (type) {
+    case 'number':
+      callback = (row1, row2) => {
+        return row1.cells[column].innerText - row2.cells[column].innerText;
+      };
+      break;
+
+    case 'string':
+      callback = (row1, row2) => {
+        return row1.cells[column].innerText.localeCompare(
+          row2.cells[column].innerText
+        );
+      };
+      break;
+  }
+
+  rows.sort(callback);
+
+  tbody.prepend(...rows);
+}
