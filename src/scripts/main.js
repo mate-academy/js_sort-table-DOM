fd12a067a07295d0184db6d323ebcd5b94991f4c
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-07 17:13:47 +0200

    destructured event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,26 +3,28 @@
 const table = document.querySelector('#cars');
 const rows = table.tBodies[0].querySelectorAll('tr');
 
-function clickHandler() {
-  if (event.target.tagName !== 'TH') {
+function clickHandler(e) {
+  const { target } = e;
+
+  if (target.tagName !== 'TH') {
     return;
   }
 
   const rowsArr = [...rows];
 
-  if (event.target.dataset.type === 'number') {
+  if (target.dataset.type === 'number') {
     rowsArr.sort((a, b) => a.cells[2].innerText - b.cells[2].innerText);
   }
 
-  if (event.target.dataset.type === 'string'
-    && event.target.innerText === 'Model') {
+  if (target.dataset.type === 'string'
+    && target.innerText === 'Model') {
     rowsArr.sort((a, b) => (
       a.cells[1].innerText.localeCompare(b.cells[1].innerText)
     ));
   }
 
-  if (event.target.dataset.type === 'string'
-    && event.target.innerText === 'Brand') {
+  if (target.dataset.type === 'string'
+    && target.innerText === 'Brand') {
     rowsArr.sort((a, b) => (
       a.cells[0].innerText.localeCompare(b.cells[0].innerText)
     ));
