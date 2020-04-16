ad1cc9ed4ced011bf58cfc8c43ee7820b69d56bd
crash2bash <sachagurov@gmail.com>
2020-04-16 07:25:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hello mate academy!');
+const cars = document.querySelector('#cars');
+const tableBody = document.querySelector('tbody');
+
+cars.addEventListener('click', (evt) => {
+  const target = evt.target.closest('TH');
+  const column = target.cellIndex;
+
+  if (!target || !cars.contains(target)) {
+    return;
+  }
+
+  const tableRows = [...tableBody.children];
+
+  if (target.dataset.type === 'number') {
+    tableRows.sort((a, b) => {
+      return a.cells[column].innerText - b.cells[column].innerText;
+    });
+  }
+
+  if (target.dataset.type === 'string') {
+    tableRows.sort((a, b) => {
+      return (a.cells[column].innerText
+        .localeCompare(b.cells[column].innerText));
+    });
+  }
+
+  tableBody.append(...tableRows);
+});
