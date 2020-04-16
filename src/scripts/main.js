e4fa8a52f86292cb4ddd0919f88077314a205a36
Andreas_Just <andreas.wq@gmail.com>
2020-04-16 21:12:14 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.querySelector('#cars');
+let sortedByIndex = -1;
+let sign = 1;
+
+table.tHead.addEventListener('click', (click) => {
+  const tbody = table.tBodies[0];
+  const th = click.target.closest('th[data-type]');
+
+  if (!th || !table.contains(th)) {
+    return;
+  }
+
+  sign = (th.cellIndex === sortedByIndex) ? -sign : 1;
+  sortedByIndex = th.cellIndex;
+
+  const getValue = row => row.cells[sortedByIndex].textContent;
+  const sortingFunctionsMap = {
+    number: (a, b) => +getValue(a) > +getValue(b) ? sign : -sign,
+    string: (a, b) => getValue(a) > getValue(b) ? sign : -sign,
+  };
+
+  const sortWith = sortingFunctionsMap[th.dataset.type];
+  const sortedRows = [...tbody.rows].sort(sortWith);
+
+  tbody.append(...sortedRows);
+});
