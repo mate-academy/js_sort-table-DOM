85309ecc71813bd27e0a1bb7b077ac4bd0a782f1
Oleg <fasfas7971@gmail.com>
2020-04-16 20:09:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-console.log('hello mate academy!');
+const cars = document.querySelector('#cars');
+
+init(cars);
+
+function init(cars) {
+  const thead = cars.querySelector('thead');
+  const rowsContainer = cars.querySelector('tbody');
+
+  thead.addEventListener('click', event => {
+    const clickedTh = event.target.closest('th');
+
+    if (!clickedTh) {
+      return;
+    }
+
+    const column = clickedTh.cellIndex;
+    const typeRow = clickedTh.dataset.type;
+    const rows = [...rowsContainer.children];
+
+    const sortVal = (val) => {
+      return val.cells[column].textContent;
+    };
+
+    rows.sort((a, b) => {
+      switch (typeRow) {
+        case 'number' :
+          return sortVal(a) - sortVal(b);
+
+        case 'string' :
+          return sortVal(a).localeCompare(sortVal(b));
+
+        default:
+          return 1;
+      }
+    });
+
+    for (const row of rows) {
+      rowsContainer.appendChild(row);
+    }
+  });
+}
