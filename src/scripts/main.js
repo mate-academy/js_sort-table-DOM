7466aea105376fe9e758990f530c045d21b3161c
AnnieMarkina <anniemurkina@gmail.com>
2020-04-17 13:42:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('hello mate academy!');
+function makeSortable(table) {
+  const thead = table.querySelector('thead');
+  const tbody = table.querySelector('tbody');
+
+  thead.addEventListener('click', evnt => {
+    const th = evnt.target.closest('th');
+
+    if (!th) {
+      return;
+    };
+
+    const column = th.cellIndex;
+    const sortType = th.dataset.sortType;
+    const rows = [...tbody.children];
+
+    rows.sort((rowA, rowB) => {
+      const valueA = rowA.cells[column].textContent;
+      const valueB = rowB.cells[column].textContent;
+
+      switch (sortType) {
+        case 'number':
+          return (+valueA) - (+valueB);
+
+        case 'string':
+          return (valueA > valueB) ? 1 : -1;
+
+        default:
+          return 0;
+      }
+    });
+
+    for (const row of rows) {
+      tbody.appendChild(row);
+    }
+  });
+};
+
+makeSortable(document.querySelector('#cars'));
