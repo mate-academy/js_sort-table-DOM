71b37a0bccdb425a33038e1cb27fa46fdb5e0e55
BorisovecViktor <viktorborisovets@gmail.com>
2020-04-18 22:31:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+
+tHead.addEventListener('click', e => {
+  const th = e.target.closest('th');
+  const col = th.cellIndex;
+  const typeOfCol = th.dataset.type;
+  const rows = [...tBody.children];
+
+  rows.sort((prevRow, nextRow) => {
+    const a = prevRow.cells[col].textContent;
+    const b = nextRow.cells[col].textContent;
+
+    if (typeOfCol === 'number') {
+      return +a - +b;
+    }
+
+    if (typeOfCol === 'string') {
+      return a.localeCompare(b);
+    }
+  });
+
+  for (const row of rows) {
+    tBody.append(row);
+  }
+});
