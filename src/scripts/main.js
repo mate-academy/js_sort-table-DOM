57a3dc65d172a990a0fe259c8c39c969b61c7f0d
Roma <roman.mykhalchuk@yahoo.com>
2020-04-20 19:15:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.getElementById('cars');
+const tBodyArray = Array.from(table.rows).slice(1);
+
+let sortedTable = [];
+
+table.addEventListener('click', (e) => {
+  if (e.target.dataset === 'string') {
+    sortedTable = tBodyArray.sort((a, b) =>
+      a.cells[e.target.cellIndex].textContent.localeCompare(
+        b.cells[e.target.cellIndex]
+      )
+    );
+  } else {
+    sortedTable = tBodyArray.sort((a, b) =>
+      a.cells[e.target.cellIndex].textContent
+      > b.cells[e.target.cellIndex].textContent
+        ? 1
+        : -1
+    );
+  }
+  table.tBodies[0].append(...sortedTable);
+});
