180308a93495aeb2937d039637af488265235097
Kira Marks <serafita22@gmail.com>
2020-04-20 18:51:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('hello mate academy!');
+function makeSortable(table) {
+  const head = document.querySelector('[data-header]');
+  const body = document.querySelector('[data-rows]');
+  let currentColumn = null;
+  let currentSign = 1;
+
+  head.addEventListener('click', (e) => {
+    const headTitle = e.target.closest('[data-type]');
+
+    if (!headTitle) {
+      return;
+    }
+
+    const column = headTitle.dataset.column;
+    const rows = [...body.children];
+
+    currentSign = (column !== currentColumn) ? 1 : -currentSign;
+
+    currentColumn = column;
+
+    const getText = (row) => {
+      return row.children[currentColumn].textContent;
+    };
+
+    rows.sort((rowA, rowB) => {
+      const sortType = headTitle.dataset.type;
+
+      switch (sortType) {
+        case 'number':
+          return currentSign * (getText(rowA) - getText(rowB));
+        case 'string':
+          return currentSign * (getText(rowA).localeCompare(getText(rowB)));
+      }
+    });
+
+    for (const row of rows) {
+      body.append(row);
+    }
+  });
+}
+
+makeSortable(document.querySelector('#cars'));
