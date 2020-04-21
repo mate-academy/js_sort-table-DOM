d326604e6cc40ca4af728bdd3e8478aa8dc028ca
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-04-21 12:25:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('hello mate academy!');
+function tableSort(table) {
+  const thead = table.querySelector('thead');
+  const tbody = table.querySelector('tbody');
+
+  thead.addEventListener('click', (e) => {
+    const th = e.target.closest('th');
+    const column = th.cellIndex;
+    const sortType = th.dataset.type;
+    const rows = [...tbody.children];
+
+    if (!th) {
+      return;
+    }
+
+    const getVal = (row) => {
+      return row.children[column].textContent;
+    };
+
+    rows.sort((a, b) => {
+      switch (sortType) {
+        case 'number':
+          return getVal(a) - getVal(b);
+        case 'string':
+          return getVal(a).localeCompare(getVal(b));
+        default:
+          return 0;
+      }
+    });
+
+    for (const row of rows) {
+      tbody.appendChild(row);
+    }
+  });
+}
+
+tableSort(
+  document.querySelector('#cars')
+);
