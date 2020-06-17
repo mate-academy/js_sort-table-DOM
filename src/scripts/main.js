5459bdd8e99e818aa803de94dedf7798cdf6ef03
kvalenty <krushunin89@gmail.com>
2020-06-17 20:12:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+
+tHead.addEventListener('click', function(e) {
+  const index = e.target.cellIndex;
+
+  sortTable(index);
+});
+
+function sortTable(index) {
+  const items = [ ...tBody.rows ];
+
+  items.sort((currentEl, nextEl) => {
+    const current = currentEl.cells[index].textContent;
+    const next = nextEl.cells[index].textContent;
+
+    return current.localeCompare(next);
+  });
+
+  tBody.append(...items);
+}
