256a5cd9d0bf4a77a23d228d117b950d2e18fca4
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-17 17:34:11 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('hello mate academy!');
+const head = document.querySelector('thead');
+const content = document.querySelector('tbody');
+const rowList = [...content.children];
+
+head.addEventListener('click', sortItems);
+
+function sortItems(e) {
+  const th = e.target;
+  const index = th.cellIndex;
+
+  let compare;
+
+  switch (e.target.dataset.type) {
+    case 'string':
+      compare = (a, b) => a.children[index].textContent
+        .localeCompare(b.children[index].textContent);
+      break;
+    case 'number':
+      compare = (a, b) => (
+        a.children[index].textContent - b.children[index].textContent);
+      break;
+  };
+
+  rowList.sort(compare);
+
+  content.append(...rowList);
+};
