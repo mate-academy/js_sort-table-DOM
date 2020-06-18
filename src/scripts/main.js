a43f331e96dd7ee67c2eea0ffd784a3d8986212a
Denys <gryshko_denys@gmail.com>
2020-06-18 10:00:08 +0300

    js solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.getElementById('cars');
+const tBody = document.querySelector('tbody');
+const rows = [...tBody.children];
+const head = document.querySelector('tr');
+
+table.addEventListener('click', sort);
+
+function sort(e) {
+  const items = e.target;
+
+  if (items.tagName !== 'TH') {
+    return;
+  }
+
+  let sorted;
+
+  for (let i = 0; i < head.children.length; i++) {
+    if (items === head.children[i]) {
+      sorted = rows
+        .sort((a, b) =>
+          (a.children[i]).textContent.localeCompare(b.children[i].textContent));
+    }
+  }
+
+  for (const element of sorted) {
+    tBody.append(element);
+  }
+}
