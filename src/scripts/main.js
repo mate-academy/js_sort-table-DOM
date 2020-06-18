e88f573357113da943cd9dda0167b6433a9eb56e
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-06-18 14:05:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.querySelector('#cars');
+const tBody = document.querySelector('tbody');
+const rows = [...tBody.children];
+
+function sort(e) {
+  const items = e.target.cellIndex;
+
+  rows.sort((a, b) =>
+    (a.children[items]).innerHTML.localeCompare(b.children[items].innerHTML));
+
+  for (const list of rows) {
+    tBody.append(list);
+  }
+}
+
+table.addEventListener('click', sort);
