4d6ab4634bb1ae08bdd5defc2ba5716a4ecd4ce1
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-19 08:40:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
 
-console.log('hello mate academy!');
+const cars = document.querySelector('#cars');
+const body = cars.tBodies[0].rows;
+
+console.log(body);
+
+cars.addEventListener('click', click => {
+  if (click.target.tagName === 'TH') {
+    const col = click.target.cellIndex;
+    const type = click.target.dataset.type;
+    const compareNumbers = (a, b) => {
+      if (type === 'number') {
+        return a[col] - b[col];
+      } else {
+        return a[col]
+          .localeCompare(b[col]);
+      }
+    };
+
+    let newBody = [];
+
+    for (let row = 0; row < body.length; row++) {
+      newBody[row] = [];
+
+      for (let cell = 0; cell < body[row].cells.length; cell++) {
+        newBody[row][cell] = body[row].cells[cell].textContent;
+      }
+    }
+    newBody = newBody.sort(compareNumbers);
+
+    for (let row = 0; row < body.length; row++) {
+      for (let cell = 0; cell < body[row].cells.length; cell++) {
+        body[row].cells[cell].textContent
+          = newBody[row][cell];
+      }
+    }
+  }
+});
