d9fc52c03de270b503e434831266597b6634f9fa
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-19 08:58:41 +0300

    two-directional sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 
 const cars = document.querySelector('#cars');
 const body = cars.tBodies[0].rows;
+let sort = 'ascend';
 
 console.log(body);
 
@@ -10,7 +11,7 @@ cars.addEventListener('click', click => {
   if (click.target.tagName === 'TH') {
     const col = click.target.cellIndex;
     const type = click.target.dataset.type;
-    const compareNumbers = (a, b) => {
+    const compareAscend = (a, b) => {
       if (type === 'number') {
         return a[col] - b[col];
       } else {
@@ -18,7 +19,14 @@ cars.addEventListener('click', click => {
           .localeCompare(b[col]);
       }
     };
-
+    const compareDescend = (a, b) => {
+      if (type === 'number') {
+        return b[col] - a[col];
+      } else {
+        return b[col]
+          .localeCompare(a[col]);
+      }
+    };
     let newBody = [];
 
     for (let row = 0; row < body.length; row++) {
@@ -28,7 +36,14 @@ cars.addEventListener('click', click => {
         newBody[row][cell] = body[row].cells[cell].textContent;
       }
     }
-    newBody = newBody.sort(compareNumbers);
+
+    if (sort === 'ascend') {
+      newBody = newBody.sort(compareAscend);
+      sort = 'descend';
+    } else if (sort === 'descend') {
+      newBody = newBody.sort(compareDescend);
+      sort = 'ascend';
+    }
 
     for (let row = 0; row < body.length; row++) {
       for (let cell = 0; cell < body[row].cells.length; cell++) {
