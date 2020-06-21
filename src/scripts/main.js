9988b95177319284aeef0b6c33d75be23a7c7290
Slava Polishchuk <dizerllc@gmail.com>
2020-06-21 12:06:23 +0300

    added css styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,40 +10,29 @@
 
 function sortTableByColumn(table, column, asc = true) {
   const dirModifier = asc ? 1 : -1;
-  const tBody = table.tBodies[0];
-  const rows = Array.from(tBody.querySelectorAll('tr'));
+  const tBody = table.querySelector('tbody');
+  const rows = [...tBody.children];
 
   // Sort each row
   const sortedRows = rows.sort((a, b) => {
-    const aColText = a
-      .querySelector(`td:nth-child(${column + 1})`)
-      .textContent.trim();
-    const bColText = b
-      .querySelector(`td:nth-child(${column + 1})`)
-      .textContent.trim();
-    // console.log(aColText);
-    // console.log(bColText);
+    const aColText = a.cells[column].innerText;
+    const bColText = b.cells[column].innerText;
 
     return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
   });
-
-  // Remove all existing TRs from the table
-  while (tBody.firstChild) {
-    tBody.removeChild(tBody.firstChild);
-  }
 
   // Re-add the newly sorted rows
   tBody.append(...sortedRows);
 
   // Remember how the column is currently sorted
-  // table.querySelectorAll('th')
-  //   .forEach(th => th.classList
-  //     .remove('th-sort-asc', 'th-sort-desc'));
+  table.querySelectorAll('th')
+    .forEach(th => th.classList
+      .remove('th-sort-asc', 'th-sort-desc'));
 
-  table.querySelector(`th:nth-child(${column + 1})`)
+  table.querySelector('thead').firstElementChild.cells[column]
     .classList.toggle('th-sort-asc', asc);
 
-  table.querySelector(`th:nth-child(${column + 1})`)
+  table.querySelector('thead').firstElementChild.cells[column]
     .classList.toggle('th-sort-desc', !asc);
 }
 
