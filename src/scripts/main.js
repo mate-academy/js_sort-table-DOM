0f2d77f1084adca3da68afb92565054f474914d6
Andrew Nickyforov <nik_andrey@ukr.net>
2020-06-21 16:13:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('hello mate academy!');
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
+const cars = [...tbody.children];
+
+table.addEventListener('click', () => {
+  switch (event.target.textContent) {
+    case 'Brand':
+      cars.sort((a, b) => {
+        return a.cells[0].innerHTML.localeCompare(
+          b.cells[0].innerHTML
+        );
+      });
+      break;
+    case 'Model':
+      cars.sort((a, b) => {
+        return a.cells[1].innerHTML.localeCompare(
+          b.cells[1].innerHTML
+        );
+      });
+      break;
+    case 'Year':
+      cars.sort((a, b) => {
+        return a.cells[2].innerHTML - b.cells[2].innerHTML;
+      });
+      break;
+  }
+
+  cars.forEach(row => tbody.append(row));
+});
